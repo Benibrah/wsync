ae1052bdc2ab2ec31e1d31f590d9d7ba9e53f535
Benibrah <48657610+Benibrah@users.noreply.github.com>
2019-03-18 03:29:14 +0100

    Mapping calculation and conversion

--- tsync.lua
+++ tsync.lua
@@ -10,11 +10,22 @@ local web = require("internet") -- Load Internet lib
 local json = require("json") -- Load JSON module
 local debug = require("component").debug -- Get debug card
 
---Get Time data from IP
-local timeData = web.request("http://worldtimeapi.org/api/ip")
-local timeTable = json.decode(timeData())
-
-local timeString timeTable["datetime"]
+--Map real time to ingame time
+mapTime = function(t)
+  return ((t-6)/(24))*24000 % 24000
+end
 
 function start()
+  while true do
+    
+    --Get Time data from IP
+    local timeData = web.request("http://worldtimeapi.org/api/ip")
+    local timeTable = json.decode(timeData())
+    
+    --Get exact hour
+     local hour = string.sub(timeTable["datetime"],12,13)
+     
+    debug.runCommand("set time " .. tonumber(string.format("%18.0f", mapTime(hour))))
+    os.sleep(10)
+  end
 end
